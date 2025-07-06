b72bbe2afedb9cb6e0ac959650011fb4dd3ff5bb
naveen@galaxy4 <naveenacad89@gmail.com>
2025-07-06 12:52:37 +0000

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
+def index(request):
+    return render(request,"blog/index.html")
+
+def detail(request,post_id):
+    return render(request,"blog/detail.html")
+
+def old_url_redirect(request):
+    return redirect(reverse("blog:new_page_url"))
+
+def new_url_view(request):
+    return HttpResponse("This is the new URL")
--- myapp/settings.py
+++ myapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    "/blog/static/blog"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
