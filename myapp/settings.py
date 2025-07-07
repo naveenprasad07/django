ac0487a443a4053dc5f5f4df2f6c1b0325db68fe
naveen@galaxy4 <naveenacad89@gmail.com>
2025-07-07 10:38:11 +0000

    custom404

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uyi(y6##a*byyh+gy6@xlpysbgs52%6k%q%%^z%4x81-nl0=rt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,17 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING  = {
+    "version":1,
+    "disable_existing_loggers":False,
+    "handlers":{
+        "console":{
+            "class":"logging.StreamHandler"
+        }
+    },
+    "root":{
+        "handlers":["console"],
+        "level":"DEBUG"
+    }
+}
